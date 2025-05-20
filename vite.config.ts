2cee294bb3643363a935de049c4f54f80749efb9
DongGyun <wooheuy5752@gmail.com>
2025-05-20 10:46:23 +0900

    chore: tailwind 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 import svgr from 'vite-plugin-svgr'
@@ -5,5 +6,5 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths(), svgr({ include: '**/*.svg?react' })],
+  plugins: [react(), tailwindcss(), tsconfigPaths(), svgr({ include: '**/*.svg?react' })],
 })
