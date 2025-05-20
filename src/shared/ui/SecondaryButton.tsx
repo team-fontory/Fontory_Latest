22071b81371198dc1c254d02f245adee123b23a9
DongGyun <wooheuy5752@gmail.com>
2025-05-20 22:53:04 +0900

    feat: SecondaryButton 구현

---
+++ src/shared/ui/SecondaryButton.tsx
@@ -0,0 +1,30 @@
+import type { ButtonHTMLAttributes, ReactNode } from 'react'
+
+import { cn } from '../lib'
+
+type Variant = 'filled' | 'outlined'
+
+type Props = {
+  children: ReactNode
+  variant?: Variant
+} & ButtonHTMLAttributes<HTMLButtonElement>
+
+const variantClassMap: Record<Variant, string> = {
+  filled: 'bg-secondary text-white hover:bg-secondary-hover',
+  outlined: 'bg-white text-secondary border-2 border-secondary hover:bg-secondary hover:text-white',
+}
+
+export const SecondaryButton = ({ className, children, variant = 'filled', ...props }: Props) => {
+  return (
+    <button
+      className={cn(
+        'rounded-small font-secondary-button px-7 py-5 text-center transition-colors',
+        variantClassMap[variant],
+        className,
+      )}
+      {...props}
+    >
+      {children}
+    </button>
+  )
+}
--- src/shared/ui/index.ts
+++ src/shared/ui/index.ts
@@ -1,2 +1,3 @@
 export * from './Icon/Icon'
 export * from './PrimaryButton'
+export * from './SecondaryButton'
