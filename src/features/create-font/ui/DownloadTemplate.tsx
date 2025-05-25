b6ba54ba3bdcc314b4dbcfc4295366e43b4a2d7d
DongGyun <wooheuy5752@gmail.com>
2025-05-25 17:27:25 +0900

    feat: 폰트 제작 이미지 속성 변경

--- src/features/create-font/ui/DownloadTemplate.tsx
+++ src/features/create-font/ui/DownloadTemplate.tsx
@@ -3,8 +3,11 @@ import { SecondaryButton } from '@/shared/ui'
 export const DownloadTemplate = () => {
   return (
     <div className="flex-column items-center gap-[3.25rem]">
-      {/* 이미지로 변경 */}
-      <div className="rounded-large bg-grey h-[28.4375rem] w-[28.4375rem]" />
+      <img
+        src={'/template/font_template.png'}
+        alt="폰트 작성 템플릿"
+        className="rounded-large border-grey h-[28.4375rem] w-[28.4375rem] border object-contain"
+      />
 
       <a href="/template/font_template.png" download="폰트 제작 템플릿.png" className="w-full">
         <SecondaryButton variant="outlined" className="w-full">
