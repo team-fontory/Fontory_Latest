4d8a9cdb8964e9347821d137650f6a7ec62feed0
DongGyun <wooheuy5752@gmail.com>
2025-05-25 16:17:43 +0900

    fix: 폰트 이름 중복 체크 오타 수정

--- src/features/check-font-name-duplicate/api/checkFontNameDuplicate.mutation.ts
+++ src/features/check-font-name-duplicate/api/checkFontNameDuplicate.mutation.ts
@@ -2,7 +2,7 @@ import { apiClient } from '@/app/api'
 import { useAxiosMutation } from '@/shared/hooks'
 
 export const endpoints = {
-  validateName: (fontName: string) => `/fonts/verify-name?fontName${fontName}`,
+  validateName: (fontName: string) => `/fonts/verify-name?fontName=${fontName}`,
 } as const
 
 export const useValidateFontName = () =>
