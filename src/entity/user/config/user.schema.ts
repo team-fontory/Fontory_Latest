addd21297b0cb133d91c33774bfae344a49d986c
DongGyun <wooheuy5752@gmail.com>
2025-05-26 20:41:38 +0900

    feat: 회원가입 스키마 힌트 문구 생성

--- src/entity/user/config/user.schema.ts
+++ src/entity/user/config/user.schema.ts
@@ -7,10 +7,12 @@ export const userAttribute = {
     section: 'nickname',
     label: '닉네임',
     placeholder: '닉네임을 입력해주세요.',
+    hint: '최대 9글자까지 입력 가능',
   },
   gender: {
     section: 'gender',
     label: '성별',
+    hint: '성별을 선택해주세요.',
   },
   birth: {
     section: 'birth',
@@ -19,6 +21,7 @@ export const userAttribute = {
     inputMode: 'numeric',
     pattern: '\\d{4}-\\d{2}-\\d{2}',
     placeholder: '생년월일을 입력해주세요.',
+    hint: '"-"를 제외한 숫자만 입력 가능',
     onInput: (e: React.FormEvent<HTMLInputElement>) => {
       const input = e.currentTarget
       input.value = formatDateInput(input.value)
@@ -33,12 +36,15 @@ export const userSchema = z.object({
   nickname: z
     .string()
     .min(1, { message: '닉네임을 입력해주세요.' })
-    .max(8, { message: '닉네임은 8자 이내로 입력해주세요.' }),
+    .max(9, { message: '9글자까지 입력 가능합니다.' }),
 
   gender: z
     .enum(['MALE', 'FEMALE'])
     .nullable()
     .refine((val) => val !== null, { message: '성별을 선택해주세요.' }),
 
-  birth: z.string().min(1, { message: '생년월일을 입력해주세요.' }),
+  birth: z
+    .string()
+    .min(10, { message: '생년월일을 모두 입력해주세요.' })
+    .max(10, { message: '생년월일을 모두 입력해주세요.' }),
 })
