fae53d1b06fb40cbf927be6f2489a1b1a03af206
DongGyun <wooheuy5752@gmail.com>
2025-05-28 13:28:44 +0900

    fix: 폰트 상세 페이지에서 제작한 다른 폰트가 있을 때만 해당 섹션 보이기

--- src/pages/FontDetailPage.tsx
+++ src/pages/FontDetailPage.tsx
@@ -19,7 +19,7 @@ const FontDetailPage = () => {
         <FontDetail {...fontDetail} />
       </section>
 
-      {recommendList && recommendList.length && (
+      {recommendList && !!recommendList.length && (
         <section className="mt-60">
           <SectionHeader title="OTHER FONTS" />
           <FontDetailOtherFonts recommendList={recommendList} />
