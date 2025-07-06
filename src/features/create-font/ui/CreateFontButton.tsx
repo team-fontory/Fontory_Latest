c1dac5a7f8628f112271a64c955ae72d0b941b1f
DongGyun <wooheuy5752@gmail.com>
2025-07-06 16:15:59 +0900

    feat: 폰트 제작 완료 후, 모든 상태 초기화

--- src/features/create-font/ui/CreateFontButton.tsx
+++ src/features/create-font/ui/CreateFontButton.tsx
@@ -1,17 +1,23 @@
 import { useFormContext } from 'react-hook-form'
 
 import type { CreateFontFormType } from '@/entity/font'
+import { useVerificationActions } from '@/features/check-font-name-duplicate'
 import { PrimaryButton } from '@/shared/ui'
 
 import { useCreateFontForm } from '../hook/useCreateFontForm'
+import { useCreateFontStepActions } from '../model/createFontStep.store'
 
 export const CreateFontButton = () => {
   const { handleSubmit, reset } = useFormContext<CreateFontFormType>()
   const { handleSubmitForm } = useCreateFontForm()
+  const { resetVerification } = useVerificationActions()
+  const { resetStep } = useCreateFontStepActions()
 
   const onSubmit = (formData: CreateFontFormType) => {
     handleSubmitForm(formData)
+    resetVerification()
     reset()
+    resetStep()
   }
 
   return (
--- src/pages/CreateFont.tsx
+++ src/pages/CreateFont.tsx
@@ -1,12 +1,15 @@
+import { useEffect } from 'react'
 import { FormProvider } from 'react-hook-form'
 
 import { type CreateFontFormType, createFontSchema, defaultValues } from '@/entity/font'
+import { useVerificationActions } from '@/features/check-font-name-duplicate'
 import {
   CreateFontStepOne,
   CreateFontStepThree,
   CreateFontStepTwo,
   stepLabels,
   useCreateFontStep,
+  useCreateFontStepActions,
   useCreateFontTotalSteps,
 } from '@/features/create-font'
 import { useCustomForm } from '@/shared/hooks'
@@ -31,13 +34,24 @@ const CreateFont = () => {
   const totalSteps = useCreateFontTotalSteps()
   const formMethods = useCustomForm<CreateFontFormType>(createFontSchema, { defaultValues })
 
+  const { resetVerification } = useVerificationActions()
+  const { resetStep } = useCreateFontStepActions()
+
+  useEffect(() => {
+    return () => {
+      formMethods.reset()
+      resetVerification()
+      resetStep()
+    }
+  }, [formMethods, resetStep, resetVerification])
+
   return (
     <Layout hasPadding>
       <SectionHeader title="CREATION" />
       <StepProgressBar
         currentStep={currentStep}
         totalSteps={totalSteps}
-        label={stepLabels[currentStep]}
+        label={stepLabels[currentStep - 1]}
       />
 
       <FormProvider {...formMethods}>
