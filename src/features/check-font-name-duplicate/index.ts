b9678612fbd4bdaa8641f8fb371d51a6ef57752c
DongGyun <wooheuy5752@gmail.com>
2025-07-06 16:06:10 +0900

    test: disabled 테스트

--- src/features/check-font-name-duplicate/index.ts
+++ src/features/check-font-name-duplicate/index.ts
@@ -1,1 +1,2 @@
+export * from './model/fontNameVerification.store'
 export * from './ui/CheckFontNameDuplicate'
--- src/features/create-font/ui/CreateFontStepTwo.tsx
+++ src/features/create-font/ui/CreateFontStepTwo.tsx
@@ -1,8 +1,7 @@
 import { useFormContext, useWatch } from 'react-hook-form'
 
 import { fontAttribute } from '@/entity/font'
-import { CheckFontNameDuplicate } from '@/features/check-font-name-duplicate'
-import { useVerificationStatus } from '@/features/check-nickname-duplicate/model/nicknameVerification.store'
+import { CheckFontNameDuplicate, useVerificationStatus } from '@/features/check-font-name-duplicate'
 import { Input, PrimaryButton, Textarea } from '@/shared/ui'
 
 import { useCreateFontStepActions } from '../model/createFontStep.store'
@@ -19,12 +18,8 @@ export const CreateFontStepTwo = () => {
   const fontEngName = useWatch({ control, name: fontAttribute.engName.section })
   const example = useWatch({ control, name: fontAttribute.example.section })
 
-  console.log(fontName, fontEngName, example, isVerified)
   const isPartialValid = !!fontName && !!fontEngName && !!example && isVerified
 
-  const a = () => {
-    console.log(fontName, fontEngName, example, isVerified)
-  }
   const onClickNextButton = async () => {
     const isValid = await trigger([
       fontAttribute.name.section,
@@ -50,10 +45,6 @@ export const CreateFontStepTwo = () => {
 
       <div className="flex-between-center mt-[6.25rem]">
         <CreateFontPrevButton prevPageNumber={1} />
-
-        <button type="button" onClick={a}>
-          aa
-        </button>
 
         <PrimaryButton direction="right" disabled={!isPartialValid} onClick={onClickNextButton}>
           다음 단계
