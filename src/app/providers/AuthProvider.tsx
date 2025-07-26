94c744bde971cbe1227d33b8f269d9aaefe6606e
DongGyun <wooheuy5752@gmail.com>
2025-07-26 12:16:19 +0900

    fix: 주석 제거

--- src/app/providers/AuthProvider.tsx
+++ src/app/providers/AuthProvider.tsx
@@ -9,7 +9,6 @@ export const AuthProvider = ({ children }: { children: ReactNode }) => {
 
   const authValue = useMemo(() => user?.userData ?? null, [user])
 
-  console.log('authValue', authValue)
   return <AuthContext.Provider value={authValue}>{children}</AuthContext.Provider>
 }
 
